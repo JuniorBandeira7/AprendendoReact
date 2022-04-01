5d98aaeb9a203b998ced2d981ea1fba2550eb9ae
Julio Bandeira <juliobandeira77@gmail.com>
2022-04-01 14:15:58 -0300

    nao lembro

---
+++ app02/src/App.js
@@ -0,0 +1,12 @@
+import React from 'react';//importa a biblioteca do react
+import Header from './componentes/Header'
+
+export default function App(){
+
+
+    return(
+    <>
+        <Header/>
+    </>
+  )
+}
---
+++ app02/src/componentes/Header.js
@@ -0,0 +1,10 @@
+import React from "react";
+
+export default function Header(){
+    return(
+        <header>
+            <p>Olá Mundo</p>
+            <h1>Criando componentes em react</h1>
+        </header>
+    )
+}
