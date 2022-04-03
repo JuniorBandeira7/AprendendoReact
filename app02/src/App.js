2c2c48b6b9b5cb99564439194982fb0a9d9e423e
Julio Bandeira <juliobandeira77@gmail.com>
2022-04-03 14:24:07 -0300

    aula criando componentes

--- app02/src/App.js
+++ app02/src/App.js
@@ -1,12 +1,14 @@
 import React from 'react';//importa a biblioteca do react
-import Header from './componentes/Header'
+import Header from './componentes/Header'/*importa o header do arquivo header da pasta componentes*/
+import Corpo from './componentes/corpo'/*o nome deve ser o mesmo aqui e no return*/
 
 export default function App(){
 
 
     return(
     <>
-        <Header/>
+        <Header/>{/*importa o header do arquivo header da pasta componentes*/}
+        <Corpo/>{/*perceba que aqui é um section, porém escreve-se o nome do import, com letra maiuscula.*/}
     </>
   )
 }
---
+++ app02/src/componentes/corpo.js
@@ -0,0 +1,9 @@
+import React from "react";
+
+export default function corpo(){
+    return(
+        <section>
+            <h1>Aqui é um section</h1>
+        </section>
+    )
+}
